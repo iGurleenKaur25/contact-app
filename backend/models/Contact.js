1b7cbff272a161796c1470ad6e7e178a55353757
igurleenkaur25 <igurleenkaur25@gmail.com>
2026-01-02 20:24:49 +0530

    initial coo+mmit

---
+++ backend/models/Contact.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema({
+  name: { type: String,
+     required: true },
+  email: String,
+  phone: { type: String,
+     required: true },
+  message: String
+});
+
+module.exports = mongoose.model("Contact", contactSchema);
