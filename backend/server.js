fbf5be5589df894ac335f38a3eacab4d66d39898
igurleenkaur25 <igurleenkaur25@gmail.com>
2026-01-03 16:04:45 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,8 @@ const contactRoutes = require("./routes/contactRoutes");
 app.use("/api/contacts", contactRoutes);
 
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
